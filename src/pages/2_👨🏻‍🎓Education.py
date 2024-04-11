121a1df79de63457cc9a0f7fd6d51a7d06678e0d
Borislove <abirme@yandex.ru>
2024-04-11 08:05:00 +0300

    Update 2_👨🏻‍🎓Education.py

--- src/pages/2_👨🏻‍🎓Education.py
+++ src/pages/2_👨🏻‍🎓Education.py
@@ -56,7 +56,7 @@ else:
 
 # image = Image.open('education.jpg')
 #st.image(image='\salary-analysis-643-py\src\pages\education.jpg', caption='education')
-st.image('education.jpg', caption='education')
+st.image('src\pages\education.jpg', caption='education')
 
 
 st.write('''
